29479cec367c164bb17973ac2c122faed54889d9
Adrizen <36826976+Adrizen@users.noreply.github.com>
2022-06-18 12:52:42 -0300

    show player and enemy on screen

--- js/index.js
+++ js/index.js
@@ -1,2 +1,55 @@
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
+
+canvas.width = 1024;
+canvas.height = 576;
+
+c.fillRect(0, 0, canvas.width, canvas.height);
+
+class Sprite {
+    constructor({ position, velocity }) {
+        this.position = position;
+        this.velocity = velocity;
+    }
+
+    draw() {
+        c.fillStyle = 'red';
+        c.fillRect(this.position.x, this.position.y, 50, 150);
+    }
+
+    update(){
+        this.draw();
+        this.position.y += 10;
+    }
+}
+
+const player = new Sprite({
+    position: {
+        x: 0,
+        y: 0
+    },
+    velocity: {
+        x: 0,
+        y: 0        
+    }
+});
+
+const enemy = new Sprite({
+    position: {
+        x: 400,
+        y: 100
+    },
+    velocity: {
+        x: 0,
+        y: 0        
+    }
+});
+
+function animate() {
+    window.requestAnimationFrame(animate);
+    c.fillRect(0, 0, canvas.width, canvas.height);
+    player.update();
+    enemy.update();
+}
+
+animate();
